c563bd41bb75cf0a29feb34b54febf9cb2154892
Pinku Nishad <161450660+081PinkuNishad@users.noreply.github.com>
2024-05-05 14:30:58 +0530

    Add files via upload

---
+++ tut61.js
@@ -0,0 +1,8 @@
+// let div=document.getElementsByClassName('container')[0]
+// div.onmouseenter=()=>{
+//     alert("hey this is external script file")
+// }
+// let btn=document.getElementsByTagName('button')[0]
+// btn.onclick=()=>{
+//     alert("hey this is external script file")
+// }
